bdf038f7c8568a05496d26ce7aae9665a9dba87a
gustav485 <gustavdambo@hotmail.dk>
2025-11-25 16:22:53 +0100

    whats up dog

--- src/simulate.c
+++ src/simulate.c
@@ -17,10 +17,10 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct 
     switch (systemkald){
         case 1:
             regs[10] = getchar();
-
+            break;
         case 2:
             putchar(regs[10]);
-
+            break;
         case 3:
             break;
         case 93:
@@ -29,6 +29,39 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct 
         default:
             printf("Error, ukendt systemkald: %u", systemkald);
             break;
+    }
+    switch (opcode) {
+        case 0x33: 
+            decode_R()
+            break;
+        case 0x13:
+            decode_I()
+            break;
+        case 0x03: 
+            decode_I()
+            break;
+        case 0x23:
+            decode_s()
+            break;
+        case 0x63:
+            decode_B()
+            break;
+        case 0x6F:
+            decode_J()
+            break;
+        case 0x67:
+            decode_I()
+            break;
+        case 0x17:
+            decode_U()
+            break;
+        case 0x37:
+            decode_U()
+            break;
+        default: 
+            printf("Ukendt opcode: 0x%x\n", opcode);
+            done = true;
+        break;
     }
 
 }
