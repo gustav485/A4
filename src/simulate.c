c6aac73c67f34ab33afb04d540f07be0ce17ae04
Frederik Rømer <frederik@MacBook-Air-6.local>
2025-11-25 15:37:08 +0100

    Mere til simulate

--- src/simulate.c
+++ src/simulate.c
@@ -1,3 +1,13 @@
 #include <memory.h>
 #include <simulate.h>
 #include <stdio.h>
+
+struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct symbols* symbols){
+    int32_t regs[32] = {0};
+    int program_counter = start_addr;
+    long int instruction_count = 0;
+    if (log_file != NULL){
+        printf("Hej");
+
+    }
+}
