c2f3b6bf89370199d5440339505b99bb130cfcde
gustav485 <gustavdambo@hotmail.dk>
2025-12-03 11:45:41 +0100

    R-type dissaaasss

--- src/disassemble.c
+++ src/disassemble.c
@@ -25,117 +25,96 @@ void disassemble(uint32_t addr, uint32_t instruction, char* result, size_t buf_s
     u_int32_t funct7 = (instruction >> 25) & 0x7F;
 
     switch(opcode) {
-                   case 0x33: {//R-type (add, sub, and, or, slt, mul …)
+            case 0x33: {//R-type (add, sub, and, or, slt, mul …)
                 switch (funct3) {
                     case 0x0: {
                         if (funct7 == 0x0) { //add
-                            regs[rd] = regs[rs1] + regs[rs2];
+                            snprintf(result, buf_size, "add %s %s %s", regname(rd), regname(rs1), regname(rs2));
                         }
                         else if (funct7 == 0x1) { //mul
-                            regs[rd] = regs[rs1] * regs[rs2];                            
+                            snprintf(result, buf_size, "mul %s %s %s", regname(rd), regname(rs1), regname(rs2));                            
                         }
                         else if (funct7 == 0x20) { //sub
-                            regs[rd] = regs[rs1] - regs[rs2];
+                            snprintf(result, buf_size, "sub %s %s %s", regname(rd), regname(rs1), regname(rs2));
                         }
                         break;
                     }
                     case 0x1:{ //sll
                         if (funct7 == 0x0){ //sll
-                            regs[rd] = regs[rs1] << regs[rs2];
+                            snprintf(result, buf_size, "sll %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1) { //mulh
-                            regs[rd] = (u_int32_t) regs[rs1] * (u_int32_t) regs[rs2];
+                            snprintf(result, buf_size, "mulh %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                     }
                     case 0x2: { 
                         if (funct7 == 0x0){ //slt
-                            regs[rd] = (regs[rs1] < regs[rs2]);
+                            snprintf(result, buf_size, "slt %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1){ //mulhsu
-                            int64_t a = (int64_t)(int32_t)regs[rs1];
-                            int64_t b = (int64_t)(uint32_t)regs[rs2];
-                            int64_t result = a * b;
-                            if (rd != 0) {
-                                regs[rd] = (int32_t)(result >> 32);  // høje 32 bit
+                            snprintf(result, buf_size, "mulhsu %s %s %s", regname(rd), regname(rs1), regname(rs2));  // høje 32 bit
                             }
                             break;
                         }
                     }
                     case 0x3: {
                         if (funct7 == 0x0) { //sltu
-                            if (rd != 0) {
-                                regs[rd] = ((uint32_t)regs[rs1] < (uint32_t)regs[rs2]) ? 1 : 0;
-                            }
+                            snprintf(result, buf_size, "sltu %s %s %s", regname(rd), regname(rs1), regname(rs2));
                         }
                         else if (funct7 == 0x1){ //mulhu
-                            uint64_t a = (uint64_t)(uint32_t)regs[rs1];
-                            uint64_t b = (uint64_t)(uint32_t)regs[rs2];
-                            uint64_t result = a * b;
-                            if (rd != 0) {
-                                regs[rd] = (uint32_t)(result >> 32);  // høje 32 bit
-                            }
+                            snprintf(result, buf_size, "mulhu %s %s %s", regname(rd), regname(rs1), regname(rs2));
                         }
                     }
                     case 0x4: { 
                         if (funct7 == 0x0) { //xor
-                            regs[rd] = regs[rs1] ^ regs[rs2];
+                            snprintf(result, buf_size, "xor %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1){ //div
-                            if (regs[rs2] == 0) {
-                                regs[rd] = -1;
-                            }
-                            else {
-                                regs[rd] = regs[rs1] / regs[rs2];
-                            }
+                            snprintf(result, buf_size, "div %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                     }
                     case 0x5: {
                         if (funct7 == 0x0){ //srl
-                            regs[rd] = regs[rs1] >> regs[rs2];
+                            snprintf(result, buf_size, "srl %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x20){ //sra
-                            regs[rd] = regs[rs1] >> regs[rs2];
+                            snprintf(result, buf_size, "sra %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1){ //divu
-                            if (regs[rs2] == 0) {
-                                regs[rd] = -1;
-                            }
-                            else {
-                            regs[rd] = (uint32_t)regs[rs1] / (uint32_t)regs[rs2];
-                            }
+                            snprintf(result, buf_size, "divu %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                     }
                     case 0x6: { //
                         if (funct7 == 0x0) { //or
-                            regs[rd] = regs[rs1] | regs[rs2];
+                            snprintf(result, buf_size, "or %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1){ //rem
-                            regs[rd] = regs[rs1] % regs[rs2];
+                            snprintf(result, buf_size, "rem %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                     }
                     case 0x7: {
                         if (funct7 == 0x0) { //and
-                            regs[rd] = regs[rs1] & regs[rs2];
+                            snprintf(result, buf_size, "and %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                         else if (funct7 == 0x1){ //remu
-                            regs[rd] = (uint32_t)regs[rs1] % (uint32_t)regs[rs2];
+                            snprintf(result, buf_size, "remu %s %s %s", regname(rd), regname(rs1), regname(rs2));
                             break;
                         }
                     }
                 }
-            break;
-        }
+                break;
+            }
         case 0x13: {//I-type (addi, xori, ori, slti, slli, srli, srai …)
             int32_t immI = (int32_t)instruction >> 20; //de første 12 bits skal stores i imm[11:0]
             uint32_t shamt = immI & 0x1F;
